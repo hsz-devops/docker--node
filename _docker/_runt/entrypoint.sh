df84c2453fa40ae34eab536ce7d0ade7dc2fb0a1
Joao C Costa <joaocc-dev@live.com>
2017-11-10 15:32:50 +0000

    small fix

--- _docker/_runt/entrypoint.sh
+++ _docker/_runt/entrypoint.sh
@@ -4,15 +4,15 @@
 #
 set -e
 set -o pipefail
-set -x
+#set -x
 pwd
 
 [ "${ENTRYPOINT_ROOT_DIR}" == "" ] || cd "${ENTRYPOINT_ROOT_DIR}"
 
 # using exec to transfer PID 1 to whatever is executed
-if [ -e ./entrypoint-cmd.sh ]; then
+if [ -e /opt/runt/entrypoint-cmd.sh ]; then
     # hook for something else
-    exec ./entrypoint-cmd.sh "$@"
+    exec /opt/runt/entrypoint-cmd.sh "$@"
 else
     exec "$@"
 fi
