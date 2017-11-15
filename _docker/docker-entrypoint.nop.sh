4758e2b7182e891c786b8b0fae8e2f5e64f8b577
Joao C Costa <joaocc-dev@live.com>
2017-11-15 23:41:54 +0000

    added azure support to nop image

--- _docker/docker-entrypoint.nop.sh
+++ _docker/docker-entrypoint.nop.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 #
-# v1.4.2    2017-11-10    webmaster@highskillz.com
+# v1.7.1    2017-11-13    webmaster@highskillz.com
 #
 set -e
 set -o pipefail
 pwd
 echo "Entering /docker-entrypoint.sh [nop] with args [$@]"
 
+# instructions for Azure AppService on Linux from
+# https://docs.microsoft.com/en-us/azure/app-service/containers/tutorial-custom-docker-image
 set -x
+if [ "${AZ__OPENSSH_SERVER__ENABLE}" == "1" ]; then
+    if [ -n "${AZ__OPENSSH_SERVER__PASSWD}" ]; then
+        # configuring SSH to start
+        echo "${AZ__OPENSSH_SERVER__PASSWD}" | chpasswd
+        echo "cd $(pwd)" >> /etc/bash.bashrc
+        ssh-keygen -A
+        /usr/sbin/sshd -f /etc/ssh/sshd_config
+    fi
+fi
+
 if [ "${ENTRYPOINT_ROOT_DIR}" != "" ]; then
     cd "${ENTRYPOINT_ROOT_DIR}"
     pwd
